d7adc2b6e3ee2a05474564bf0d6b3dbc4a9e77b5
Florian <florian@Florians-Mac-Pro.local>
2020-03-09 18:11:16 +0100

    Video + seethru

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ const { src, dest, parallel } = require('gulp');
 const concat = require('gulp-concat');
 
 function js() {
-  return src(['./node_modules/jquery/dist/jquery.min.js',
-  './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  return src(['./node_modules/bootstrap/dist/js/bootstrap.min.js',
   './www/dev/js/app.js'],
   { sourcemaps: true })
     .pipe(concat('app.min.js'))
